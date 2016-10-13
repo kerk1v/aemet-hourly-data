83fd5c4cbbdc086403147b760f256f5a24cf8de7
Volker Kerkhoff <vkerkhoff@riplife.es>
2016-10-13 16:41:12 +0200

    Add some logic to retrieve values prior to the last

--- aemet-get.py
+++ aemet-get.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 # encoding: utf-8
+import sys
 import csv
 import urllib2
+
+manylines=int(sys.argv[1])
+if manylines > 24:
+    print "The maximum of last values that can be retrieved from AEMET is 24"
+    sys.exit(254)
 
 with open('station.id') as stnfile:
     stations = csv.reader(stnfile, delimiter=';')
@@ -13,7 +19,9 @@ with open('station.id') as stnfile:
         curlUri='http://www.aemet.es/es/eltiempo/observacion/ultimosdatos_' + stationId + '_datos-horarios.csv?k=and&l=' + stationId + '&datos=det&w=0&f=temperatura&x=h24'
         urlResponse = urllib2.urlopen(curlUri)
         aemetData = urlResponse.read()
-        aemetDataLast = aemetData.splitlines()[4]
-        lineOut = '"' + stationProv + '",' + '"' + stationId + '",' + '"' + stationLoc + '",' + aemetDataLast
-        print lineOut
+        lastline = manylines+4
+        for thisline in range (4,lastline,1):
+            aemetDataLast = aemetData.splitlines()[thisline]
+            lineOut = '"' + stationProv + '",' + '"' + stationId + '",' + '"' + stationLoc + '",' + aemetDataLast
+            print lineOut
         
