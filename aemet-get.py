e17cab6cb2a0fe19f6ed3dd81daf552e4cd0575c
volkerk <volkerk@elasticfront.spainip.es>
2016-10-13 23:20:00 +0200

    Added a unique ID to every data set generated from the date and the Station ID. Useful because I plan to import the data into ElasticSearch for analysis. Will also allow us to catch up id data comes late

--- aemet-get.py
+++ aemet-get.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
 # encoding: utf-8
+import StringIO
 import sys
 import csv
 import urllib2
+import string
+import re
 
-manylines=int(sys.argv[1])
+manylines=1
+
+if len(sys.argv)==2:
+    manylines=int(sys.argv[1])
+
+if len(sys.argv)>2:
+    print "Usage: ./aemet-get.py [number of last values to retrieve]"
+    sys.exit(254)
+
 if manylines > 24:
     print "The maximum of last values that can be retrieved from AEMET is 24"
     sys.exit(254)
@@ -23,5 +34,12 @@ with open('station.id') as stnfile:
         for thisline in range (4,lastline,1):
             aemetDataLast = aemetData.splitlines()[thisline]
             lineOut = '"' + stationProv + '",' + '"' + stationId + '",' + '"' + stationLoc + '",' + aemetDataLast
-            print lineOut
-        
+            # now let's separate into csv, we need this to build the unique ID for ElasticSearch
+            dataLine = StringIO.StringIO(lineOut)
+            csvData = csv.reader(dataLine, delimiter=',')
+            for csvDataLine in csvData: 
+                rawId = csvDataLine[3] + csvDataLine[1]
+		id=filter(str.isalnum, rawId)
+            #and now for constructing our CSV output and printing it, first field is ID
+            outLine = '"' + id + '",' + lineOut
+            print outLine
